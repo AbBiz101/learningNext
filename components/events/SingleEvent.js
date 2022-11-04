cca7103782e5e674484395995f4048c46adab40e
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-04 01:36:42 +0100

    event added

--- components/events/SingleEvent.js
+++ components/events/SingleEvent.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
-import classes from "./event-item.module.css"
+import Button from '../button/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import classes from './event-item.module.css';
 export default function SingleEvent(props) {
 	const { id, title, description, location, image, date, time, isFeatured } =
 		props;
@@ -18,14 +21,21 @@ export default function SingleEvent(props) {
 				<div className={classes.summary}>
 					<h2>{title}</h2>
 					<div className={classes.date}>
+						<DateIcon />
 						<time>{humanDate}</time>
 					</div>
 					<div className={classes.address}>
+						<AddressIcon />
 						<address>{formattedAddress}</address>
 					</div>
 				</div>
 				<div className={classes.actions}>
-					<Link href={url}>Explore Event</Link>
+					<Button link={url}>
+						<span>Explore Event</span>
+						<span className={classes.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
 				</div>
 			</div>
 		</li>
--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,5 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from './../../components/event-detail/event-summary';
+import EventLogistics from './../../components/event-detail/event-logistics';
+import EventContent from './../../components/event-detail/event-content';
 export default function EventDetail() {
-  return <div>EventDetail</div>;
+	const router = useRouter();
+	const eventID = router.query.id;
+	const event = getEventById(eventID);
+console.log(event);
+	if (!event) {
+		return <div>No event found!</div>;
+	}
+
+	return (
+		<>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				image={event.image}
+				imageAlt={event.title}
+				address={event.location}
+			/>
+			<EventContent>
+				<p>{event.description}</p>
+			</EventContent>
+		</>
+	);
 }
