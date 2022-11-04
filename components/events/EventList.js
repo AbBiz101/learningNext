a07d4d8a4ccb5005c6a446e4da58e49d5dda83e1
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-04 01:04:12 +0100

    css logic added

--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import SingleEvent from './SingleEvent';
-
+import classes from "./event-list.module.css"
 export default function EventList(props) {
 	const { items } = props;
 	return (
-		<div>
+		<div className={classes.list}>
 			{items.map((event) => (
 				<SingleEvent
 					key={event.id}
--- components/events/SingleEvent.js
+++ components/events/SingleEvent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-
+import classes from "./event-item.module.css"
 export default function SingleEvent(props) {
 	const { id, title, description, location, image, date, time, isFeatured } =
 		props;
@@ -12,30 +12,22 @@ export default function SingleEvent(props) {
 	const url = `/event/${id}`;
 	const formattedAddress = location.replace(',', '\n');
 	return (
-		<li>
+		<li className={classes.item}>
 			<img src={'/' + image} alt={title} />
-			<div>
-				<div>
+			<div className={classes.content}>
+				<div className={classes.summary}>
 					<h2>{title}</h2>
-					<div>
+					<div className={classes.date}>
 						<time>{humanDate}</time>
 					</div>
-					<div>
+					<div className={classes.address}>
 						<address>{formattedAddress}</address>
 					</div>
 				</div>
-				<div>
-					<Link href={url}></Link>
+				<div className={classes.actions}>
+					<Link href={url}>Explore Event</Link>
 				</div>
 			</div>
 		</li>
 	);
 }
-
-// 		title: 'Programming for everyone',
-// 		description:
-// 			'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
-// 		location: 'Somestreet 25, 12345 San Somewhereo',
-// 		date: '2021-05-12',
-// 		image: 'images/coding-event.jpg',
-// 		isFeatured: false,
